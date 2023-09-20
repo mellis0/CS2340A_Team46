748f25156c51cebbeb46de1493c91384e00b48f2
mellis0 <matt1.ellis1@hotmail.com>
2023-09-20 18:48:16 -0400

    exit button working

--- app/src/main/java/com/example/cs2340a_team46/MainActivity.java
+++ app/src/main/java/com/example/cs2340a_team46/MainActivity.java
@@ -3,7 +3,8 @@ package com.example.cs2340a_team46;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
-
+import android.widget.Button;
+import android.widget.RadioGroup;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.start_screen);
+        Button startBtn = findViewById(R.id.startButton);
+        Button exitBtn = findViewById(R.id.exitButton);
+
+        // TODO: implement transition from start screen to game
+//        startBtn.setOnClickListener(v -> {
+//            Intent game = new Intent(MainActivity.this, GameActivity.class);
+//            startActivity(game);
+//            finish();
+//        });
+
+        exitBtn.setOnClickListener(v -> {
+            MainActivity.this.finish();
+            System.exit(0);
+        });
     }
 }
