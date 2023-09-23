b56c90df632fbf93cd57b034c40782e7492d8fbb
Giovanni Hsu <giovannihsu.123@gmail.com>
2023-09-23 14:04:52 -0400

    initial

---
+++ app/src/main/java/com/example/cs2340a_team46/GameActivity.java
@@ -0,0 +1,15 @@
+package com.example.cs2340a_team46;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.widget.Button;
+
+public class GameActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.config_screen);
+    }
+}
