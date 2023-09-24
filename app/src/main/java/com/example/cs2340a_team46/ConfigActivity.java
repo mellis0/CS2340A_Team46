717657539b740d1f700444c5640dca867f00ac45
gtrotsuka <otsukaryan@gmail.com>
2023-09-24 14:23:07 -0400

    Update game_screen

--- app/src/main/java/com/example/cs2340a_team46/ConfigActivity.java
+++ app/src/main/java/com/example/cs2340a_team46/ConfigActivity.java
@@ -4,12 +4,93 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
+import android.widget.EditText;
 public class ConfigActivity extends AppCompatActivity {
+    private RadioGroup characterRadioGroup;
+    private RadioGroup difficultyRadioGroup;
+    private ImageView imageView;
+    private EditText nameEditText;
+    private int difficulty;
+    private int character;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.config_screen);
+
+        nameEditText = findViewById(R.id.editText);
+
+        Button startGameBtn = findViewById(R.id.startGameButton);
+
+        startGameBtn.setOnClickListener(v -> {
+            String enteredName = nameEditText.getText().toString().trim();
+            if (enteredName.isEmpty()) {
+                // Display a message if the name is empty
+                Toast.makeText(ConfigActivity.this,
+                        "Please enter a name.",
+                        Toast.LENGTH_SHORT).show();
+            } else if (difficulty == 0) {
+                Toast.makeText(ConfigActivity.this,
+                        "Please select difficulty.",
+                        Toast.LENGTH_SHORT).show();
+            } else if (character == 0) {
+                Toast.makeText(ConfigActivity.this,
+                        "Please select a character.",
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                Intent game = new Intent(ConfigActivity.this, GameActivity.class);
+                game.putExtra("name", enteredName);
+                game.putExtra("difficulty", difficulty);
+                game.putExtra("character", character);
+                startActivity(game);
+                finish();
+            }
+
+        });
+
+        characterRadioGroup = findViewById(R.id.characterRadioGroup);
+        imageView = findViewById(R.id.imageView);
+
+        // Set a listener for the RadioGroup
+        characterRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                // Handle the radio button selection
+                RadioButton selectedRadioButton = findViewById(checkedId);
+
+                if (selectedRadioButton.getId() == R.id.characterRadioOption1) {
+                    imageView.setImageResource(R.drawable.angel);
+                    character = 1;
+                } else if (selectedRadioButton.getId() == R.id.characterRadioOption2) {
+                    imageView.setImageResource(R.drawable.knight);
+                    character = 2;
+                } else if (selectedRadioButton.getId() == R.id.characterRadioOption3) {
+                    imageView.setImageResource(R.drawable.lizard);
+                    character = 3;
+                }
+            }
+        });
+
+        difficultyRadioGroup = findViewById(R.id.difficultyRadioGroup);
+        difficultyRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                // Handle the radio button selection
+                RadioButton selectedRadioButton = findViewById(checkedId);
+
+                if (selectedRadioButton.getId() == R.id.difficultyRadioOption1) {
+                    difficulty = 1;
+                } else if (selectedRadioButton.getId() == R.id.difficultyRadioOption2) {
+                    difficulty = 2;
+                } else if (selectedRadioButton.getId() == R.id.difficultyRadioOption3) {
+                    difficulty = 3;
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/cs2340a_team46/GameActivity.java
+++ app/src/main/java/com/example/cs2340a_team46/GameActivity.java
@@ -3,6 +3,7 @@ package com.example.cs2340a_team46;
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,27 +11,31 @@ import android.widget.EditText;
 public class GameActivity extends AppCompatActivity{
 
     protected void onCreate(Bundle savedInstanceState) {
-        //Game Screen Temp: Waiting on Config Screen
-        //Assumes Game is currently on Game Screen
         super.onCreate(savedInstanceState);
-        Button GStoESBtn = findViewById(R.id.gs_es);
-        String difficulty = getIntent().getStringExtra("difficulty");
-        String name = getIntent().getStringExtra("name");
+//        getSupportActionBar().hide();
+//        setContentView(R.layout.game_screen);
+//        Button GStoESBtn = findViewById(R.id.gs_es);
+//        String name = getIntent().getStringExtra("name");
+//        String difficulty = getIntent().getStringExtra("difficulty");
+//        String character = getIntent().getStringExtra("Character");
+//        Log.d("name", name);
+//        Log.d("difficulty", difficulty);
+//        Log.d("character", character);
+//
+//        EditText difficultyText = findViewById(R.id.difficulty);
+//        difficultyText.setText(difficulty);
+//
+//        EditText nameText = findViewById(R.id.charName);
+//        nameText.setText(name);
 
-        EditText difficultyText = findViewById(R.id.difficulty);
-        difficultyText.setText(difficulty);
-
-        EditText nameText = findViewById(R.id.charName);
-        nameText.setText(name);
-
-        EditText health = findViewById(R.id.health);
-        health.setText("100");
-
-        GStoESBtn.setOnClickListener(v -> {
-            Intent end = new Intent(GameActivity.this, EndActivity.class); //Change according to end class
-            startActivity(end);
-            finish();
-        });
+//        EditText health = findViewById(R.id.health);
+//        health.setText("100");
+//
+//        GStoESBtn.setOnClickListener(v -> {
+//            //Intent end = new Intent(GameActivity.this, EndActivity.class); //Change according to end class
+//            //startActivity(end);
+//            //finish();
+//        });
 
 
     }
