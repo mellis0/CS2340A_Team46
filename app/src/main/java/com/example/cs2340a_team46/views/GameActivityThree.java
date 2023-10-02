0017c09e1b9d4339be79316b92eecba79c2f4d21
mellis0 <matt1.ellis1@hotmail.com>
2023-10-01 20:20:19 -0400

    working viewmodel

--- app/src/main/java/com/example/cs2340a_team46/views/GameActivityThree.java
+++ app/src/main/java/com/example/cs2340a_team46/views/GameActivityThree.java
@@ -18,47 +18,26 @@ public class GameActivityThree extends AppCompatActivity {
         getSupportActionBar().hide();
         setContentView(R.layout.game_screen_3);
 
-        String name = getIntent().getStringExtra("name");
-        int difficulty = getIntent().getIntExtra("difficulty", 0);
-        int character = getIntent().getIntExtra("character", 0);
+        String name = com.example.cs2340a_team46.viewmodels.GameViewModel.getPlayerName();;
+        String difficulty = com.example.cs2340a_team46.viewmodels.GameViewModel.getDifficulty();
+        int character = com.example.cs2340a_team46.viewmodels.GameViewModel.getCharacter();
+        int hp = com.example.cs2340a_team46.viewmodels.GameViewModel.getPlayerHealth();
 
         //Name
         TextView nameText = findViewById(R.id.name);
         nameText.setText("Name: " + name);
 
         //Difficulty
-        String diffText;
         TextView difficultyText = findViewById(R.id.difficulty);
-        if (difficulty == 1) {
-            diffText = "Easy";
-        } else if (difficulty == 2) {
-            diffText = "Normal";
-        } else {
-            diffText = "Hard";
-        }
-        difficultyText.setText("Difficulty: " + diffText);
+        difficultyText.setText("Difficulty: " + difficulty);
 
         //Health
         TextView health = findViewById(R.id.health);
-        int hp;
-        if (diffText.equals("Easy")) {
-            hp = 150;
-        } else if (diffText.equals("Normal")) {
-            hp = 100;
-        } else {
-            hp = 50;
-        }
         health.setText("Health: " + String.valueOf(hp));
 
         //Image
         ImageView avatar = findViewById(R.id.avatar);
-        if (character == 1) {
-            avatar.setImageResource(R.drawable.angel);
-        } else if (character == 2) {
-            avatar.setImageResource(R.drawable.knight);
-        } else if (character == 3) {
-            avatar.setImageResource(R.drawable.lizard);
-        }
+        avatar.setImageResource(character);
 
 
         Button nextButton = findViewById(R.id.next_button);
--- app/src/main/java/com/example/cs2340a_team46/views/GameActivityTwo.java
+++ app/src/main/java/com/example/cs2340a_team46/views/GameActivityTwo.java
@@ -18,55 +18,34 @@ public class GameActivityTwo extends AppCompatActivity {
         getSupportActionBar().hide();
         setContentView(R.layout.game_screen_2);
 
-        String name = getIntent().getStringExtra("name");
-        int difficulty = getIntent().getIntExtra("difficulty", 0);
-        int character = getIntent().getIntExtra("character", 0);
+        String name = com.example.cs2340a_team46.viewmodels.GameViewModel.getPlayerName();;
+        String difficulty = com.example.cs2340a_team46.viewmodels.GameViewModel.getDifficulty();
+        int character = com.example.cs2340a_team46.viewmodels.GameViewModel.getCharacter();
+        int hp = com.example.cs2340a_team46.viewmodels.GameViewModel.getPlayerHealth();
 
         //Name
         TextView nameText = findViewById(R.id.name);
         nameText.setText("Name: " + name);
 
         //Difficulty
-        String diffText;
         TextView difficultyText = findViewById(R.id.difficulty);
-        if (difficulty == 1) {
-            diffText = "Easy";
-        } else if (difficulty == 2) {
-            diffText = "Normal";
-        } else {
-            diffText = "Hard";
-        }
-        difficultyText.setText("Difficulty: " + diffText);
+        difficultyText.setText("Difficulty: " + difficulty);
 
         //Health
         TextView health = findViewById(R.id.health);
-        int hp;
-        if (diffText.equals("Easy")) {
-            hp = 150;
-        } else if (diffText.equals("Normal")) {
-            hp = 100;
-        } else {
-            hp = 50;
-        }
         health.setText("Health: " + String.valueOf(hp));
 
         //Image
         ImageView avatar = findViewById(R.id.avatar);
-        if (character == 1) {
-            avatar.setImageResource(R.drawable.angel);
-        } else if (character == 2) {
-            avatar.setImageResource(R.drawable.knight);
-        } else if (character == 3) {
-            avatar.setImageResource(R.drawable.lizard);
-        }
+        avatar.setImageResource(character);
 
 
         Button nextButton = findViewById(R.id.next_button);
         nextButton.setOnClickListener(v -> {
             Intent thirdScreen = new Intent(GameActivityTwo.this, GameActivityThree.class);
-            thirdScreen.putExtra("name", name);
-            thirdScreen.putExtra("difficulty", difficulty);
-            thirdScreen.putExtra("character", character);
+//            thirdScreen.putExtra("name", name);
+//            thirdScreen.putExtra("difficulty", difficulty);
+//            thirdScreen.putExtra("character", character);
 
             startActivity(thirdScreen);
             finish();
