c483fc9b78246cc46c1f03c61c7e7801591d68a3
jandrews78 <jojo@Jos-MacBook-Air-2.local>
2023-10-07 14:42:15 -0400

    made singleton player class, made changes to GameViewModel class accordingly - previous edits are commented out

--- app/src/main/java/com/example/cs2340a_team46/models/Player.java
+++ app/src/main/java/com/example/cs2340a_team46/models/Player.java
@@ -1,4 +1,24 @@
 package com.example.cs2340a_team46.models;
 
 public class Player {
+    private volatile static Player instance;
+    public static int playerHealth;
+    public static String difficulty;
+    public static String playerName;
+    public static int character;
+
+    private Player() { }
+
+    // made thread safe
+    public static Player getInstance() {
+        if (instance == null) {
+            synchronized (Player.class) {
+                if (instance == null) {
+                    instance = new Player();
+                }
+            }
+        }
+        return instance;
+    }
+
 }
--- app/src/main/java/com/example/cs2340a_team46/viewmodels/GameViewModel.java
+++ app/src/main/java/com/example/cs2340a_team46/viewmodels/GameViewModel.java
@@ -1,23 +1,29 @@
 package com.example.cs2340a_team46.viewmodels;
 
 import com.example.cs2340a_team46.R;
+import com.example.cs2340a_team46.models.Player;
 
 public class GameViewModel {
-    private static int playerHealth;
-    private static String difficulty;
-    private static String playerName;
-    private static int character;
+    // private static int playerHealth;
+    // private static String difficulty;
+    // private static String playerName;
+    // private static int character;
 
 // need to implement player singleton model first. Once implemented, we might not need the static vars above
 //    private static Player player = null;
+    // com.example.cs2340a_team46.viewmodels.GameViewModel.getPlayerName(); example of accessing methods from a class in another directory
 
+    private static Player player = Player.getInstance();
     public static void setPlayerHealth(int difficultyVal) {
         if (difficultyVal == 1) {
-            GameViewModel.playerHealth = 150;
+            player.playerHealth = 150;
+            // GameViewModel.playerHealth = 150;
         } else if (difficultyVal == 2) {
-            GameViewModel.playerHealth = 100;
+            player.playerHealth = 100;
+            // GameViewModel.playerHealth = 100;
         } else if (difficultyVal == 3) {
-            GameViewModel.playerHealth = 50;
+            player.playerHealth = 50;
+            // GameViewModel.playerHealth = 50;
         } else {
             throw new IllegalArgumentException("Difficulty must be 1, 2, or 3");
         }
@@ -25,45 +31,52 @@ public class GameViewModel {
 
     public static void setDifficulty(int difficultyVal) {
         if (difficultyVal == 1) {
-            GameViewModel.difficulty = "Easy";
+            player.difficulty = "Easy";
+            // GameViewModel.difficulty = "Easy";
         } else if (difficultyVal == 2) {
-            GameViewModel.difficulty = "Normal";
+            player.difficulty = "Normal";
+            // GameViewModel.difficulty = "Normal";
         } else if (difficultyVal == 3) {
-            GameViewModel.difficulty = "Hard";
+            player.difficulty = "Hard";
+            // GameViewModel.difficulty = "Hard";
         } else {
             throw new IllegalArgumentException("Difficulty must be 1, 2, or 3");
         }
     }
 
     public static void setPlayerName(String name) {
-        GameViewModel.playerName = name;
+        player.playerName = name;
+        // GameViewModel.playerName = name;
     }
 
     public static void setCharacter(int characterRadioButton) {
         if (characterRadioButton == 1) {
-            GameViewModel.character = R.drawable.angel;
+            player.character = R.drawable.angel;
+            // GameViewModel.character = R.drawable.angel;
         } else if (characterRadioButton == 2) {
-            GameViewModel.character = R.drawable.knight;
+            player.character = R.drawable.knight
+            // GameViewModel.character = R.drawable.knight;
         } else if (characterRadioButton == 3) {
-            GameViewModel.character = R.drawable.lizard;
+            player.character = R.drawable.lizard;
+            // GameViewModel.character = R.drawable.lizard;
         } else {
             throw new IllegalArgumentException("characterRadioButton must be 1, 2, or 3");
         }
     }
 
     public static int getPlayerHealth() {
-        return playerHealth;
+        return player.playerHealth;
     }
 
     public static String getDifficulty() {
-        return difficulty;
+        return player.difficulty;
     }
 
     public static String getPlayerName() {
-        return playerName;
+        return player.playerName;
     }
 
     public static int getCharacter() {
-        return character;
+        return player.character;
     }
 }
