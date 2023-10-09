bfdd6c4507858fb09aa9fe3738599f8c7b2a6d2b
jandrews78 <jojo@Jos-MacBook-Air-2.local>
2023-10-09 16:33:22 -0400

    changed singleton player class and GVM class - made vars private and included getters and setters

--- app/src/main/java/com/example/cs2340a_team46/models/Player.java
+++ app/src/main/java/com/example/cs2340a_team46/models/Player.java
@@ -2,10 +2,42 @@ package com.example.cs2340a_team46.models;
 
 public class Player {
     private volatile static Player instance;
-    public static int playerHealth;
-    public static String difficulty;
-    public static String playerName;
-    public static int character;
+    private int playerHealth;
+    private String difficulty;
+    private String playerName;
+    private int character;
+
+    // getter and setter for heatlh
+    public void setPlayerHealth(int playerHealth) {
+        this.playerHealth = playerHealth;
+    }
+    public int getPlayerHealth() {
+        return playerHealth;
+    }
+
+    // getter and setter for difficulty
+    public void setDifficulty(String difficulty) {
+        this.difficulty = difficulty;
+    }
+    public String getDifficulty() {
+        return difficulty;
+    }
+
+    // getter and setter for player name
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    // getter and setter for character
+    public void setCharacter(int character) {
+        this.character = character;
+    }
+    public int getCharacter() {
+        return character;
+    }
 
     private Player() { }
 
--- app/src/main/java/com/example/cs2340a_team46/viewmodels/GameViewModel.java
+++ app/src/main/java/com/example/cs2340a_team46/viewmodels/GameViewModel.java
@@ -16,13 +16,13 @@ public class GameViewModel {
     private static Player player = Player.getInstance();
     public static void setPlayerHealth(int difficultyVal) {
         if (difficultyVal == 1) {
-            player.playerHealth = 150;
+            player.setPlayerHealth(150);
             // GameViewModel.playerHealth = 150;
         } else if (difficultyVal == 2) {
-            player.playerHealth = 100;
+            player.setPlayerHealth(100);
             // GameViewModel.playerHealth = 100;
         } else if (difficultyVal == 3) {
-            player.playerHealth = 50;
+            player.setPlayerHealth(50);
             // GameViewModel.playerHealth = 50;
         } else {
             throw new IllegalArgumentException("Difficulty must be 1, 2, or 3");
@@ -31,13 +31,13 @@ public class GameViewModel {
 
     public static void setDifficulty(int difficultyVal) {
         if (difficultyVal == 1) {
-            player.difficulty = "Easy";
+            player.setDifficulty("Easy");
             // GameViewModel.difficulty = "Easy";
         } else if (difficultyVal == 2) {
-            player.difficulty = "Normal";
+            player.setDifficulty("Normal");
             // GameViewModel.difficulty = "Normal";
         } else if (difficultyVal == 3) {
-            player.difficulty = "Hard";
+            player.setDifficulty("Hard");
             // GameViewModel.difficulty = "Hard";
         } else {
             throw new IllegalArgumentException("Difficulty must be 1, 2, or 3");
@@ -45,19 +45,19 @@ public class GameViewModel {
     }
 
     public static void setPlayerName(String name) {
-        player.playerName = name;
+        player.setPlayerName(name);
         // GameViewModel.playerName = name;
     }
 
     public static void setCharacter(int characterRadioButton) {
         if (characterRadioButton == 1) {
-            player.character = R.drawable.angel;
+            player.setCharacter(R.drawable.angel);
             // GameViewModel.character = R.drawable.angel;
         } else if (characterRadioButton == 2) {
-            player.character = R.drawable.knight
+            player.setCharacter(R.drawable.knight)
             // GameViewModel.character = R.drawable.knight;
         } else if (characterRadioButton == 3) {
-            player.character = R.drawable.lizard;
+            player.setCharacter(R.drawable.lizard);
             // GameViewModel.character = R.drawable.lizard;
         } else {
             throw new IllegalArgumentException("characterRadioButton must be 1, 2, or 3");
@@ -65,18 +65,18 @@ public class GameViewModel {
     }
 
     public static int getPlayerHealth() {
-        return player.playerHealth;
+        return player.getPlayerHealth();
     }
 
     public static String getDifficulty() {
-        return player.difficulty;
+        return player.getDifficulty();
     }
 
     public static String getPlayerName() {
-        return player.playerName;
+        return player.getPlayerName();
     }
 
     public static int getCharacter() {
-        return player.character;
+        return player.getCharacter();
     }
 }
