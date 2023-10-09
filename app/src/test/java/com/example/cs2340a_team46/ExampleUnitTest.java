f0e57844f8e30fea130e4e8453113d69d59e2098
melodiehsieh <mhsieh60@gatech.edu>
2023-10-08 23:37:06 -0400

    created basic leaderboard backing code

--- app/src/test/java/com/example/cs2340a_team46/ExampleUnitTest.java
+++ app/src/test/java/com/example/cs2340a_team46/ExampleUnitTest.java
@@ -4,6 +4,9 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import com.example.cs2340a_team46.models.LeaderboardEntry;
+import java.util.Date;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -14,4 +17,7 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+
+
 }
+
